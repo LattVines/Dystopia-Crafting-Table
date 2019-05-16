43af35de21a147b79be1a49bc6638e22ca6f13c8
Latt Vines <lattv@hoover.lib.al.us>
2019-05-15 20:19:55 -0500

    Added better toggle button support
    buttons in Attack and Defend panel now toggle correctly

---
+++ Assets/ToggleButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleButton : MonoBehaviour
+{
+
+    Image img;
+    public Color Deactivated_Color = Color.grey;
+    public Color Active_Color = Color.yellow;
+
+    void Awake()
+    {
+        img = GetComponent<Image>();
+    }
+
+    public void Toggle(bool b)
+    {
+        if (b) img.color = Active_Color;
+        else img.color = Deactivated_Color;//
+    }
+
+}
