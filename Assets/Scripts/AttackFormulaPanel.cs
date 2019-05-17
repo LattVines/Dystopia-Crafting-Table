08dbfb839e3277d9e6d8754d6e0ae9333047e38c
Latt Vines <lattv@hoover.lib.al.us>
2019-05-17 15:05:02 -0500

    Attack & Defense Formulas now work
    Attack & Defense Formulas now work

--- Assets/Scripts/AttackFormulaPanel.cs
+++ Assets/Scripts/AttackFormulaPanel.cs
@@ -1,18 +1,109 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class AttackFormulaPanel : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public string ability = string.Empty;
+    public string scaler = string.Empty;
+    public int constant_bonus = 0;
+    public InputField constantBonusInput;//SET IN EDITOR
+    public Text formula_text;//SET IN EDITOR
+
     void Start()
     {
-        
+        UpdateFormulaText();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void SetAbilityBonus(string s)
     {
-        
+        ability = s;
+        UpdateFormulaText();
     }
+
+    public void SetScalerBonus(string s)
+    {
+        scaler = s;
+        UpdateFormulaText();
+    }
+
+    public void UpdateConstantBonus()
+    {
+        constant_bonus = int.Parse(constantBonusInput.text.ToString()); ;
+        UpdateFormulaText();
+    }
+
+
+    public void UpdateFormulaText()
+    {
+
+        int max = 0; int min = 0;
+
+        //these ability values are used for determining the max & min.
+        int brawn = 0; int brains = 0; int agility = 0;
+
+        string ability_string = string.Empty;
+        string constant_bonus_string = string.Empty;
+        string formula;
+
+        //---------------- ability score stuff ----------
+        if (ability == "Brawn")
+        {
+            brawn += 10;
+        }
+        if (ability == "Brains")
+        {
+            brains += 10;
+        }
+        if (ability == "Agility")
+        {
+            agility += 10;
+        }
+
+
+        if (ability != string.Empty)
+        {
+            ability_string = ability + " + ";
+        }
+        //------------------------------------------------
+
+
+        //constant value stuff ----------------------------
+        if(constant_bonus > 0)
+        {
+            constant_bonus_string = " + " + constant_bonus.ToString();
+        }
+        else if (constant_bonus < 0)
+        {
+            constant_bonus_string = " - " + Mathf.Abs(constant_bonus).ToString();
+        }
+        //------------------------------------------------
+
+
+        //-------------------------- scaler --------------
+        string scaler_string = string.Empty;
+        if (scaler != string.Empty)
+        {
+            scaler_string = " + " + scaler;
+        }
+        //-----------------------------------------------
+
+        max = brawn + brains + agility + constant_bonus + 10;
+        min = 0 + 0 + 0 + constant_bonus + 1;//agility + brawn + brains + constant bonus + roll of 1 + at least level 1 if scaler used
+        if (scaler != string.Empty)
+        {
+            max += 10;
+            min += 1;
+        }
+
+        formula = "Attack Formula:\n" + ability_string + "d10" + scaler_string + constant_bonus_string
+            + "\nMaximum Roll: " + max.ToString()
+            + "\nMinimum Roll: " + min.ToString();
+
+        formula_text.text = formula;
+    
+    }
+
+
 }
+
--- Assets/Scripts/DefenderFormulaPanel.cs
+++ Assets/Scripts/DefenderFormulaPanel.cs
@@ -1,18 +1,132 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class DefenderFormulaPanel : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public string ability = string.Empty;
+    public string challenge_used = string.Empty;
+    public int constant_bonus = 0;
+    public InputField constantBonusInput;//SET IN EDITOR
+    public Text formula_text;//SET IN EDITOR
+
     void Start()
     {
-        
+        UpdateFormulaText();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void SetAbilityBonus(string s)
     {
-        
+        ability = s;
+        UpdateFormulaText();
     }
+
+    public void SetChallengeBonus(string s)
+    {
+        challenge_used = s;
+        UpdateFormulaText();
+    }
+
+    public void UpdateConstantBonus()
+    {
+        constant_bonus = int.Parse(constantBonusInput.text.ToString());
+        UpdateFormulaText();
+    }
+
+
+    public void UpdateFormulaText()
+    {
+        int max = 0; int min = 0;
+
+        //these ability values are used for determining the max & min.
+        int brawn = 0; int brains = 0; int agility = 0;
+
+        string ability_string = string.Empty;
+        string constant_bonus_string = string.Empty;
+        string formula;
+
+        //---------------- ability score stuff ----------
+        if (ability == "Brawn")
+        {
+            brawn += 10;
+        }
+        if (ability == "Brains")
+        {
+            brains += 10;
+        }
+        if (ability == "Agility")
+        {
+            agility += 10;
+        }
+
+
+        //------------------------------------------------
+
+
+        //constant value stuff ----------------------------
+        if (constant_bonus > 0)
+        {
+            constant_bonus_string = " + " + constant_bonus.ToString();
+        }
+        else if (constant_bonus < 0)
+        {
+            constant_bonus_string = " - " + Mathf.Abs(constant_bonus).ToString();
+        }
+        //------------------------------------------------
+
+
+        //-------------------------- CHALLENGE --------------
+        string challenge_String = string.Empty;
+        if (challenge_used != string.Empty)
+        {
+            challenge_String = "CHALLENGE";
+        }
+        //-----------------------------------------------
+
+        max = brawn + brains + agility + constant_bonus;
+        min = 0 + 0 + 0 + constant_bonus;
+        if (challenge_used != string.Empty)
+        {
+            max += 10;//agility + brawn + brains + constant bonus + roll of 10 on challenge if used
+            min += 1;//agility + brawn + brains + constant bonus + roll of 1
+        }
+
+        string formula_builder = string.Empty;
+
+        //case just challenge
+        if (challenge_String != string.Empty) formula_builder = challenge_String;
+
+
+        //case ability with or without challenge
+        if(ability != string.Empty && challenge_String != string.Empty)
+        {
+            formula_builder = challenge_String + " + " + ability;
+        }
+        else if(ability != string.Empty && challenge_String == string.Empty)
+        {
+            formula_builder = ability;
+        }
+
+        //case ability, challenge and constant
+        if(formula_builder == string.Empty && constant_bonus_string != string.Empty)
+        {
+            formula_builder = constant_bonus_string;
+        }
+        else if(formula_builder != string.Empty && constant_bonus_string != string.Empty)
+        {
+            formula_builder = formula_builder + constant_bonus_string;
+        }
+
+
+        formula = "Defend Formula:\n" + formula_builder
+            + "\nMaximum Roll: " + max.ToString()
+            + "\nMinimum Roll: " + min.ToString();
+
+        formula_text.text = formula;
+
+
+    }
+
+
 }
